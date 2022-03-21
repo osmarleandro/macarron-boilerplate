c8e1719ec44a231dd4d29ba4f6911128c45c60a0
Osmar Leandro <osmarleandrods@gmail.com>
2022-03-21 16:53:48 -0300

    verifing a macaroon

--- lib/macaroon-example.ts
+++ lib/macaroon-example.ts
@@ -1,5 +1,7 @@
 import * as macaroon from "macaroon";
+import Macaroon from "macaroon";
 import * as crypto from "crypto";
+
 
 let bakeMcrns = () => {
     try {
@@ -8,7 +10,8 @@ let bakeMcrns = () => {
         let identifier = new Date().toString();
 
         //Generate Macaroon
-        let accessMacaroon = macaroon.newMacaroon({ identifier: identifier, location: location, rootKey: rootKey, version: 2 });
+        let accessMacaroon: Macaroon = macaroon.newMacaroon({ identifier: identifier, location: location, rootKey: rootKey});
+
 
         return [rootKey, accessMacaroon.exportBinary()];
     } catch (error) {
@@ -16,4 +19,21 @@ let bakeMcrns = () => {
     }
 }
 
-console.log(bakeMcrns());
+let verifyMcrns = (rootKey: string, accessMacarron: Uint8Array) => {
+    try {
+        //var base64macaroon = macaroon.base64ToBytes(accessMacarron);
+        let veraccessmcrn: Macaroon = macaroon.importMacaroon(accessMacarron);
+        veraccessmcrn.verify(rootKey, () => null, []);
+
+        console.log("Authentication pass");
+
+    } catch (error) {
+        console.error("Authentication Failed!");
+        throw error;
+    }
+}
+
+
+let baked: Object[] = bakeMcrns();
+console.log(baked);
+verifyMcrns(<string>baked[0], <Uint8Array>baked[1]);
